2b72e04f65b90109fb612b965f7c8f9e6cf660f3
Analogkrd <analogkrd@gmail.com>
2025-02-24 21:43:43 +0300

    Fix <img tag issue

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,6 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Partners() {
+  const logos = [
+    "/partner1.png",
+    "/partner2.png",
+    "/partner3.png",
+    "/partner4.png",
+  ];
+
   return (
     <section className="py-16 bg-gray-100">
       <h2 className="text-4xl font-bold text-primary text-center mb-8">
@@ -8,13 +16,17 @@ export default function Partners() {
       </h2>
       <div className="overflow-hidden">
         <div className="flex animate-scroll">
-          {[
-            "/partner1.png",
-            "/partner2.png",
-            "/partner3.png",
-            "/partner4.png",
-          ].map((logo, index) => (
-            <img key={index} src={logo} alt="Partner" className="h-20 mx-6" />
+          {logos.map((logo, index) => (
+            // Wrap each image in a container with fixed width & height
+            <div key={index} className="relative h-20 w-[120px] mx-6">
+              <Image
+                src={logo}
+                alt="Partner"
+                fill
+                sizes="(max-width: 768px) 100vw, 120px"
+                className="object-contain"
+              />
+            </div>
           ))}
         </div>
       </div>
