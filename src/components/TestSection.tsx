c10d80fa8ea1cd6d5e1e37efa4a3b02b7b6556b0
Analogkrd <analogkrd@gmail.com>
2025-02-24 21:46:25 +0300

    Update TestSection.tsx

--- src/components/TestSection.tsx
+++ src/components/TestSection.tsx
@@ -1,17 +1,8 @@
-"use client"
-const images = [
-  "/images/farmer-training.jpg",
-  "/images/farmer-training.jpg",
-  "/images/farmer-training.jpg",
-  "/images/sustainable-farming.jpg",
-  "/images/farmers-kurd-2.jpg",
-];
-
 export default function TestSection() {
 
 return (
   <section className="py-16 text-center">
-
+    Awdier
   </section>
 );
 }
