9eabf8588ef2d4c71075a64e6d84c08b7cb23b57
Analogkrd <analogkrd@gmail.com>
2025-02-25 11:59:49 +0300

    Update Map.tsx
    show Awdier location

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,7 +2,7 @@ export default function Map() {
   return (
     <iframe
       className="w-full h-full"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3219.6718145114883!2d43.96928039999999!3d36.19886259999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x400723c3f8446f87%3A0x86cb3ed161fdeec!2sAnalog%20Technology%20Agency!5e0!3m2!1sen!2siq!4v1740082721727!5m2!1sen!2siq"
+      src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3220.098584751025!2d43.96862027581056!3d36.18848417242782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzbCsDExJzE4LjUiTiA0M8KwNTgnMTYuMyJF!5e0!3m2!1sen!2siq!4v1740473887033!5m2!1sen!2siq"
       allowFullScreen
       loading="lazy"
       />
