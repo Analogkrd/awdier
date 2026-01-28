a3dc2bebc42ebc21147849e96fef0703e1133a49
Barez <barezb@gmail.com>
2026-01-28 16:53:21 +0300

    change email address to info

--- src/data/companyDetails.ts
+++ src/data/companyDetails.ts
@@ -4,7 +4,7 @@ const companyDetails = {
     address: 'Erbil, Iraq, Italian City 1 - Villa# 548',
     phone: '+964 750 671 0000',
     phone2: '+964 770 464 3636',
-    email: 'awdierorganization@gmail.com',
+    email: 'info@awdier.com',
     facebook: 'https://www.facebook.com/profile.php?id=61573012779881',
     instagram: 'https://www.instagram.com/awdier_for_agriculture',
     linkedin: 'https://www.linkedin.com/in/awdier-cooparative-asociation-acada-470086339/',
